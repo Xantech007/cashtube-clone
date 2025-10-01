da2473369592c0b1b9e6e418aa70f60b28866870
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-10-01 06:52:28 +0100

    Update and rename navbar.html to navbar.php

---
+++ inc/navbar.php
@@ -0,0 +1,10 @@
+<?php
+// navbar.php
+?>
+<nav id="ham-navigation" class="ham-menu">
+    <ul>
+        <li><a href='index.php'>Home</a></li>
+        <li class="active"><a href='about.php'>About</a></li>
+        <li><a href='contact.php'>Contact</a></li>
+    </ul>
+</nav>
