3d3e377af29b30a053fa0b94cca9e1a476f11c3c
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2026-05-01 08:03:23 +0000

    Add files via upload

---
+++ inc/translate.php
@@ -0,0 +1,16 @@
+<!-- Google Translate -->
+<div id="google_translate_element" style="margin-top: 8px;"></div>
+
+<script type="text/javascript">
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+        pageLanguage: 'en',
+        includedLanguages: 'en,fr,es,de,pt,ar,zh-CN,ru', // Add or remove languages as needed
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+        autoDisplay: false
+    }, 'google_translate_element');
+}
+</script>
+<script type="text/javascript" 
+        src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
+</script>
